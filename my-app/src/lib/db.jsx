0479028c900b18de3f5a07bca0f5e2044f73f7b7
Salmaan Shire <38257397+SalmaanShire365@users.noreply.github.com>
2024-09-18 21:56:07 -0500

    Dev Branch (#3)
    * this is my first commit! meep moop
    
    * added files
    
    * Fixed login api verification -> dashboard routes now
    
    ---------
    
    Co-authored-by: Saadaq Shire <kingenraged@gmail.com>

--- my-app/src/lib/db.jsx
+++ my-app/src/lib/db.jsx
@@ -1,14 +1,13 @@
-// src/lib/db.jsx
-import { Client } from 'pg';
+import mysql from 'mysql2/promise';
 
-// Create a new instance of the Client class
-const client = new Client({
-  connectionString: process.env.DATABASE_URL, // Use your environment variable for the connection string
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-// Connect to the database
-client.connect()
-  .then(() => console.log('Database connected successfully'))
-  .catch(err => console.error('Database connection error:', err));
-
-export default client;
+export default pool;
